8c5c07d6f0734ceec527da13f8db5581b53fd7df
fabmede <fabinhotmedeiros@gmail.com>
2022-04-25 11:19:41 -0300

    Commit inicial

---
+++ src/main/java/com/ftm/example/integration/IntegrationConfig.java
@@ -0,0 +1,44 @@
+package com.ftm.example.integration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowBuilder;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+
+import java.io.File;
+
+@Configuration
+public class IntegrationConfig {
+
+    @Autowired
+    private Transform transform;
+
+    @Bean
+    public IntegrationFlow integrationFlow() {
+        return IntegrationFlows.from(fileReader(),
+                        spec -> spec.poller(Pollers.fixedDelay(500)))
+                .transform(transform, "transform")
+                .handle(fileWriter())
+                .get();
+    }
+
+    @Bean
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("destination"));
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+    @Bean
+    public FileReadingMessageSource fileReader() {
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setDirectory(new File("source"));
+        return source;
+    }
+}
---
+++ src/main/java/com/ftm/example/integration/SpringIntegrationExampleFileadapterApplication.java
@@ -0,0 +1,13 @@
+package com.ftm.example.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationExampleFileadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationExampleFileadapterApplication.class, args);
+	}
+
+}
